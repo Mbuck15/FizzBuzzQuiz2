e4233e81415c96cd33ae6b162ecb371dea5273b3
Mason Buck <buck.217@osu.edu>
2018-03-28 19:04:20 -0400

    Kata Quiz

---
+++ FizzBuzz.Library/FizzBuzzService.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+namespace FizzBuzz.Library{
+    public class FizzBuzzService
+    {
+        public String Print(int number)
+        {
+            if (IsDivisibleBy3(number) && IsDivisibleBy5(number) )
+            {
+                return "FizzBuzz";
+            } else if (IsDivisibleBy5(number))
+            {
+                return "Buzz";
+            }else if (IsDivisibleBy3(number))
+            {
+                return "Fizz";
+            }
+
+            return number.ToString();
+        }
+        private bool IsDivisibleBy3 (int number){
+            return number % 3 ==0;
+        }
+
+        private bool IsDivisibleBy5 (int number){
+            return number % 5 ==0;
+        }
+    }
+}
